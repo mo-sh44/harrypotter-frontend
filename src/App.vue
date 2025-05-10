319185fd7d6de66a476b6e6bd3d5f3f901094908
Louris Marianna Houch <Louris-Marianna.Houch@Student.HTW-Berlin.de>
2025-05-10 17:18:03 +0200

    edit 3

--- src/App.vue
+++ src/App.vue
@@ -21,21 +21,27 @@ export default {
 </script>
 
 <style>
+body, html {
+  margin: 0;
+  padding: 0;
+  text-align: left;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  padding: 1rem;
+  padding: 1.5rem 2rem;
+  max-width: 800px;
 }
 
 header h1 {
-  text-align: left;
   font-size: 2rem;
   margin: 0;
   padding: 0;
 }
 
 hr {
-  margin-top: 0.3rem;
-  margin-bottom: 1.2rem;
+  margin-top: 0.5rem;
+  margin-bottom: 1.5rem;
 }
 </style>
 
