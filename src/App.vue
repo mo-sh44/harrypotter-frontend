8c30bda279d7958d460b7e0e24386482f83806c8
Louris Marianna Houch <Louris-Marianna.Houch@Student.HTW-Berlin.de>
2025-05-10 13:52:59 +0200

    Initial commit with Vite + CharacterList

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>Harry Potter WebTech</h1>
+    <CharacterList />
+  </div>
+</template>
+
+<script>
+import CharacterList from './components/CharacterList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CharacterList
+  }
+}
+</script>
+
+<style>
+/* Optional: kleines Styling */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  padding: 1rem;
+}
+</style>
---
+++ src/components/CharacterList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>Harry Potter Charaktere</h2>
+    <ul>
+      <li v-for="char in characters" :key="char.name">
+        {{ char.name }} - {{ char.house || 'Kein Haus' }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CharacterList",
+  data() {
+    return {
+      characters: [],
+    };
+  },
+  mounted() {
+    fetch("https://spring-boot-harrypotterwebtec-7e3cd5d6a9e7.herokuapp.com/api/external/characters")
+        .then((res) => res.json())
+        .then((data) => {
+          this.characters = data;
+        })
+        .catch((err) => {
+          console.error("Fehler beim Laden:", err);
+        });
+  },
+};
+</script>
